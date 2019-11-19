f0e6b4ca13213722ba0640e6f4b211bc7533b0a7
edwardtanzhao <edwardtanzhao@gmail.com>
2019-11-19 11:29:19 -0500

    Initial upload, only sequential for merge and quick sort

---
+++ ECSE420_Project/ECSE420_Project/main.cu
@@ -0,0 +1,147 @@
+#include<stdio.h>
+#include<stdlib.h>
+// CUDA runtime
+#include<cuda_runtime.h>
+#include<device_launch_parameters.h>
+
+//quick sort
+//swap elements
+void swap(int* a, int* b) {
+	int t = *a;
+	*a = *b;
+	*b = t;
+}
+
+//quick sort
+int partition(int arr[], int low, int high)
+{
+	int pivot = arr[high]; // pivot 
+	int i = (low - 1);  // Index of smaller element 
+
+	for (int j = low; j <= high - 1; j++)
+	{
+		// If current element is smaller than the pivot 
+		if (arr[j] < pivot)
+		{
+			i++;    // increment index of smaller element 
+			swap(&arr[i], &arr[j]);
+		}
+	}
+	swap(&arr[i + 1], &arr[high]);
+	return (i + 1);
+}
+//quick sort
+void quickSort(int arr[], int low, int high)
+{
+	if (low < high)
+	{
+		/* pi is partitioning index, arr[p] is now
+		   at right place */
+		int pi = partition(arr, low, high);
+
+		// Separately sort elements before 
+		// partition and after partition 
+		quickSort(arr, low, pi - 1);
+		quickSort(arr, pi + 1, high);
+	}
+}
+
+void merge(int arr[], int l, int m, int r)
+{
+	int i, j, k;
+	const int n1 = m - l + 1;
+	const int n2 = r - m;
+
+	/* create temp arrays */
+	int L[n1], R[n2];
+
+	/* Copy data to temp arrays L[] and R[] */
+	for (i = 0; i < n1; i++)
+		L[i] = arr[l + i];
+	for (j = 0; j < n2; j++)
+		R[j] = arr[m + 1 + j];
+
+	/* Merge the temp arrays back into arr[l..r]*/
+	i = 0; // Initial index of first subarray 
+	j = 0; // Initial index of second subarray 
+	k = l; // Initial index of merged subarray 
+	while (i < n1 && j < n2)
+	{
+		if (L[i] <= R[j])
+		{
+			arr[k] = L[i];
+			i++;
+		}
+		else
+		{
+			arr[k] = R[j];
+			j++;
+		}
+		k++;
+	}
+
+	/* Copy the remaining elements of L[], if there
+	   are any */
+	while (i < n1)
+	{
+		arr[k] = L[i];
+		i++;
+		k++;
+	}
+
+	/* Copy the remaining elements of R[], if there
+	   are any */
+	while (j < n2)
+	{
+		arr[k] = R[j];
+		j++;
+		k++;
+	}
+}
+
+/* l is for left index and r is right index of the
+   sub-array of arr to be sorted */
+void mergeSort(int arr[], int l, int r)
+{
+	if (l < r)
+	{
+		// Same as (l+r)/2, but avoids overflow for 
+		// large l and h 
+		int m = l + (r - l) / 2;
+
+		// Sort first and second halves 
+		mergeSort(arr, l, m);
+		mergeSort(arr, m + 1, r);
+
+		merge(arr, l, m, r);
+	}
+}
+
+void printArray(int A[], int size)
+{
+	for (int i = 0; i < size; i++)
+		printf("%d ", A[i]);
+	printf("\n");
+}
+
+
+int main(int args, char* argv[]) {
+	//quick sort
+	int arr[] = { 10, 7, 8, 9, 1, 5 };
+	int n = sizeof(arr) / sizeof(arr[0]);
+	quickSort(arr, 0, n - 1);
+	printf("Sorted array: n");
+	printArray(arr, n);
+
+	//merge sort
+	int arr[] = { 12, 11, 13, 5, 6, 7 };
+	int arr_size = sizeof(arr) / sizeof(arr[0]);
+
+	printf("Given array is \n");
+	printArray(arr, arr_size);
+
+	mergeSort(arr, 0, arr_size - 1);
+
+	printf("\nSorted array is \n");
+	printArray(arr, arr_size);
+}
